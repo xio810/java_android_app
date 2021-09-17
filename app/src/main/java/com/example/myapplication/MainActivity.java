7ac3c52a1304b09bf7c3bb11ba85fe354176c1aa
xio810 <boi4806@naver.com>
2021-09-17 20:22:31 +0900

    결과 10 보여주기

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void btnAddClicked(View view) {
+        int rs = 10;
+        Toast.makeText(getApplicationContext(), "결과 : " + rs, Toast.LENGTH_SHORT).show();
     }
 }
