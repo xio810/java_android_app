d040a2d8f1eff481500f5254dd15cf0d9ee2c749
xio810 <boi4806@naver.com>
2021-09-19 17:20:28 +0900

    지우기 및 결과 toast 아닌 버전

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     public void btnAddClicked(View view) {
         EditText editTextNum1 = findViewById(R.id.activity_main__editTextNum1);
         EditText editTextNum2 = findViewById(R.id.activity_main__editTextNum2);
+        TextView textViewRs = findViewById(R.id.activity_main__textViewRs);
 
         //혹시나 있을 양옆 공백 제거
         editTextNum1.setText(editTextNum1.getText().toString().trim());
@@ -42,10 +44,21 @@ public class MainActivity extends AppCompatActivity {
         int num2 = Integer.parseInt(editTextNum2.getText().toString());
 
         int rs = num1 + num2;
-        toastMsg("결과 : " + rs);
+//        toastMsg("결과 : " + rs);
+        textViewRs.setText("결과 : " + rs);
     }
 
     void toastMsg(String msg) {
         Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
     }
-}
+
+    public void btnClearClicked(View view) {
+        EditText editTextNum1 = findViewById(R.id.activity_main__editTextNum1);
+        EditText editTextNum2 = findViewById(R.id.activity_main__editTextNum2);
+        TextView textViewRs = findViewById(R.id.activity_main__textViewRs);
+
+        editTextNum1.setText("");
+        editTextNum2.setText("");
+        textViewRs.setText("");
+    }
+} 
